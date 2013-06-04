2db19fb395d59c67458b55c1f0c3f27a4b4d7023
Jiyuan Lin <linjiyuan90@gmail.com>
2013-06-04 22:04:54 +0800

    similarity.cc

--- htmm/avg_plot.py
+++ htmm/avg_plot.py
@@ -7,6 +7,46 @@ from collections import defaultdict
 USAGE = \
 '''python avg_plot.py input_list theta uid_depress'''
 
+def avg_plot(nyy):
+  m = numpy.size(nyy, 1)
+  yy = [0] * m
+  for i in range(m):
+    yy[i] = numpy.average(nyy[:, i])
+  return yy
+
+def max_var_plot(nyy):
+  m = numpy.size(nyy, 1)
+  yy = [0] * m
+  for i in range(numpy.size(nyy, 0)):
+    if numpy.var(yy) < numpy.var(nyy[i,:]):
+      yy = nyy[i, :]
+  return yy
+
+def var_trend_plot(nyy, file_list):
+  n = numpy.size(nyy, 0)
+  assert n == len(file_list)
+  pos_list = range(n)
+  pos_list = sorted(pos_list, key=lambda x : (len(file_list[x]), file_list[x]))
+  yy = [0] * n
+  for (ix, p) in enumerate(pos_list):
+    yy[ix] = numpy.var(nyy[p,:])
+  return yy
+
+def kl_divergence(x, y):
+  d = 0
+  for (ix, p) in enumerate(x):
+    q = y[ix]
+    d += p*math.log(p/q, 2)
+  return d
+
+def func_kl(xx, yy, func):
+  kl = None
+  for x in xx:
+    for y in yy:
+      _kl = kl_divergence(x, y)
+      kl = func(kl, _kl) if kl else _kl
+  return kl
+
 if __name__ == '__main__':
   if len(sys.argv) != 4:
     print(USAGE)
@@ -34,23 +74,36 @@ if __name__ == '__main__':
     uid_depress[ix][1] = float(line[1])
     id = int(uid_depress[ix][0])
     # print(beg[id], end[id])
-    yy = theta[beg[id]:end[id]+1]
-    m = len(yy[0])
-    nyy = numpy.array(yy)
-    yy = [0] * m
-    '''
-    for i in range(m):
-      yy[i] = numpy.average(nyy[:, i])
-    '''
-    for i in range(numpy.size(nyy, 0)):
-      if numpy.var(yy) < numpy.var(nyy[i,:]):
-        yy = nyy[i, :]
+    nxx = numpy.array(theta[beg[id]:end[id]+1])
+    # kl
+    max_trend = []
+    min_trend = []
+    for (iy, line) in enumerate(uid_depress):
+      uid_depress[iy][1] = float(line[1])
+      idy = int(uid_depress[iy][0])
+      nyy = numpy.array(theta[beg[idy]:end[idy]+1])
+      max_trend.append(func_kl(nxx, nyy, max))
+      min_trend.append(func_kl(nxx, nyy, min))
+
     ax = subplot(n, 1, ix+1)
-    ax.plot(range(m), yy, label='%d    %f' % (id, uid_depress[ix][1]))
-    # ax.legend()
+    ax.plot(range(len(max_trend)), max_trend, label='%d    %f    max' % (id, uid_depress[ix][1]))
+    ax.plot(range(len(min_trend)), min_trend, label='%d    %f    min' % (id, uid_depress[ix][1]))
+    ax.legend()
+
+    continue
+
+    nyy = numpy.array(theta[beg[id]:end[id]+1])
+    m = numpy.size(nyy, 1)
+    # yy = avg_plot(nyy)
+    # yy = max_var_plot(nyy)
+    yy = var_trend_plot(nyy, input_list[beg[id]:end[id]+1])
+    ax = subplot(n, 1, ix+1)
+    ax.plot(range(len(yy)), yy, label='%d    %f' % (id, uid_depress[ix][1]))
     ax.legend()
 
   plt.plot()
-  plt.savefig('all_max_var.png')
+  #plt.savefig('all_max_var.png')
+  #plt.savefig('all_var_trend.png')
+  plt.savefig('kl.png')
 
 
---
+++ similarity.cc
@@ -0,0 +1,61 @@
+#include "iostream"
+#include "string"
+#include "sstream"
+#include "fstream"
+#include "algorithm"
+#include "cmath"
+#include "vector"
+
+using namespace std;
+
+double kl_divergence(vector<double> P, vector<double> Q) {
+  double sum = 0;
+  for (double p : P) {
+    for (double q : Q) {
+      sum += p * log(p/q);
+    }
+  }
+  return sum;
+}
+
+void read(const char *file, vector<vector<double> > &vd) {
+  ifstream fin;
+  fin.open(file);  //ifstream::in
+  for (string line; getline(fin, line); ) {
+    vector<double> vt;
+    double d;
+    for (stringstream ss(line); ss >> d; ) {
+      vt.push_back(d);
+    }
+    vd.push_back(vt);
+  }
+ }
+
+int main(int argc, const char* argv[]) {
+  
+  if (argc != 3) {
+    cerr << "Usage ./" << argv[0] << " file_a file_b" << endl;
+  } 
+
+  vector<vector<double> > A, B;
+  read(argv[1], A);
+  read(argv[2], B);
+
+  double max_kl = -1, min_kl = -1;
+  for (vector<double> a : A) {
+    for (vector<double> b : B) {
+      double kl = kl_divergence(a, b);
+      if (min_kl == -1 || min_kl > kl) {
+        min_kl = kl;
+      }
+      if (max_kl == -1 || max_kl < kl) {
+        max_kl = kl;
+      }
+    }
+  }
+
+  cout << min_kl << endl << max_kl << endl;
+ 
+  return 0;
+}
+
